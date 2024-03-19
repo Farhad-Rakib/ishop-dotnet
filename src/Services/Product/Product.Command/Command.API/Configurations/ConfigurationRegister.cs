c54d29b456f4ff9f747869b4120f08caf5f54ed2
Farhad Rakib <rakib.farhad@surbanajurong.com>
2024-03-20 02:00:04 +0600

    Initial Commit With Structured Folder

---
+++ src/Services/Product/Product.Command/Command.API/Configurations/ConfigurationRegister.cs
@@ -0,0 +1,23 @@
+﻿namespace Command.API.Configurations;
+
+internal static class ConfigurationRegister
+{
+    internal static ConfigureHostBuilder AddConfigurations(this ConfigureHostBuilder host)
+    {
+        host.ConfigureAppConfiguration((context, config) =>
+        {
+            const string configurationsDirectory = "Configurations";
+            var env = context.HostingEnvironment;
+            config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/logger.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/logger.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/cache.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/cache.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/database.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"{configurationsDirectory}/database.{env.EnvironmentName}.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+        });
+        return host;
+    }
+}
